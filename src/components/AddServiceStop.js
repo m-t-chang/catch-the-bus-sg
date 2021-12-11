0e45865b1984d96dbbec3803fc088b93fc097a01
mtchang <michaeltgchang@gmail.com>
2021-12-11 13:05:28 +0800

    estimated duration now uses time

--- src/components/AddServiceStop.js
+++ src/components/AddServiceStop.js
@@ -1,9 +1,22 @@
 import React, { useState } from "react";
 import Button from "@mui/material/Button";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
 
 const AddServiceStop = (props) => {
     const [serviceInput, setServiceInput] = useState("");
     const [stopInput, setStopInput] = useState("");
+    const [inputValue, setInputValue] = useState("");
+
+    const top100Films = [
+        { label: "The Shawshank Redemption", year: 1994 },
+        { label: "The Godfather", year: 1972 },
+        { label: "The Godfather: Part II", year: 1974 },
+        { label: "The Dark Knight", year: 2008 },
+        { label: "12 Angry Men", year: 1957 },
+        { label: "Schindler's List", year: 1993 },
+        { label: "Pulp Fiction", year: 1994 },
+    ];
 
     return (
         <div>
@@ -13,12 +26,35 @@ const AddServiceStop = (props) => {
                     props.handleFormSubmit(event, serviceInput, stopInput)
                 }
             >
-                <input
+                <Autocomplete
+                    value={serviceInput} // this matches the Options, in this case, an object
+                    onChange={(event, newValue) => setServiceInput(newValue)}
+                    inputValue={inputValue} //note that this is a string
+                    onInputChange={(event, newInputValue) => {
+                        setInputValue(newInputValue);
+                        console.log(inputValue);
+                    }}
+                    disablePortal
+                    id="combo-box-demo"
+                    options={top100Films}
+                    renderInput={(params) => (
+                        <TextField
+                            {...params}
+                            required
+                            variant="outlined"
+                            label="Movie"
+                        />
+                    )}
+                />
+                {serviceInput.label}
+                {serviceInput.year}
+                {inputValue}
+                {/* <input
                     type="text"
                     placeholder="Service No."
                     value={serviceInput}
                     onChange={(event) => setServiceInput(event.target.value)}
-                />
+                /> */}
                 <input
                     type="text"
                     placeholder="Stop"
--- src/components/ArrivalCard.js
+++ src/components/ArrivalCard.js
@@ -5,8 +5,35 @@ import { CardActions, CardContent, CardActionArea } from "@mui/material";
 
 const ArrivalCard = (props) => {
     // TODO: ideally this should be fetching the time instead
-    const [duration, setDuration] = useState("no data");
+    // const [duration, setDuration] = useState("no data");
+    const [nextTime, setNextTime] = useState("");
     const [arrivalObject, setArrivalObject] = useState({});
+
+    async function fetchArrivals(serviceNo, stop) {
+        const response = await fetch(
+            `https://arrivelah2.busrouter.sg/?id=${stop}`
+        );
+        const myJson = await response.json(); //extract JSON from the http response
+
+        // update the card with the returned data
+        for (let service of myJson.services) {
+            if (service.no === serviceNo) {
+                // display in mins, rounded down
+                // setDuration(Math.floor(service.next.duration_ms / 60000));
+                setNextTime(service.next.time);
+
+                // card.duration2 = Math.floor(
+                //     service.next2.duration_ms / 60000
+                // );
+                // card.duration3 = Math.floor(
+                //     service.next3.duration_ms / 60000
+                // );
+
+                setArrivalObject(service);
+            }
+        }
+        console.log("Fetch complete");
+    }
 
     // on mount, update the data
     useEffect(() => {
@@ -15,32 +42,10 @@ const ArrivalCard = (props) => {
         );
 
         // TODO: this should be in try-catch
-        async function fetchArrivals() {
-            const response = await fetch(
-                `https://arrivelah2.busrouter.sg/?id=${props.data.stop}`
-            );
-            const myJson = await response.json(); //extract JSON from the http response
-
-            // update the card with the returned data
-            for (let service of myJson.services) {
-                if (service.no === props.data.serviceNo) {
-                    // display in mins, rounded down
-                    setDuration(Math.floor(service.next.duration_ms / 60000));
-
-                    // card.duration2 = Math.floor(
-                    //     service.next2.duration_ms / 60000
-                    // );
-                    // card.duration3 = Math.floor(
-                    //     service.next3.duration_ms / 60000
-                    // );
-
-                    setArrivalObject(service);
-                }
-            }
-            console.log("Fetch complete");
-        }
-        fetchArrivals();
-    }, []);
+        // and per LTA's recommendation, if there's no arrival data, then should check whether bus is in operation,
+        //      and display a message accordingly ("Not in operation" or "no arrival data" )
+        fetchArrivals(props.data.serviceNo, props.data.stop);
+    }, [props.data.serviceNo, props.data.stop]);
 
     return (
         <Card onClick={props.handleCardOnClick}>
@@ -49,7 +54,16 @@ const ArrivalCard = (props) => {
                     <h2>
                         {props.data.serviceNo} at Stop: {props.data.stop}
                     </h2>
-                    <p>Mins: {duration}</p>
+                    {/* <p>Mins: {duration}</p>
+                    <p>Arrives at: {nextTime}</p>
+                    <p>as Unix: {Date.parse(nextTime)}</p>
+                    <p>Time right now: {Date.now()}</p> */}
+                    <p>
+                        mins until:{" "}
+                        {Math.floor(
+                            (Date.parse(nextTime) - Date.now()) / 60000
+                        )}
+                    </p>
                 </CardContent>
             </CardActionArea>
             <CardActions>
