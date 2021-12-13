9343b2df55950b36faef4e7329c4d3e06ab18b1e
mtchang <michaeltgchang@gmail.com>
2021-12-13 11:13:41 +0800

    clean up example text for Autocomplete

--- src/components/AddServiceStop.js
+++ src/components/AddServiceStop.js
@@ -10,16 +10,6 @@ const AddServiceStop = (props) => {
     const [inputValue, setInputValue] = useState("");
     const staticData = useContext(StaticDataContext);
 
-    const top100Films = [
-        { label: "The Shawshank Redemption", year: 1994 },
-        { label: "The Godfather", year: 1972 },
-        { label: "The Godfather: Part II", year: 1974 },
-        { label: "The Dark Knight", year: 2008 },
-        { label: "12 Angry Men", year: 1957 },
-        { label: "Schindler's List", year: 1993 },
-        { label: "Pulp Fiction", year: 1994 },
-    ];
-
     let busStopAutocomplete = "";
     if (staticData.busStops.data) {
         busStopAutocomplete = (
